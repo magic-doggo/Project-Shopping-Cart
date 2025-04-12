6a97115a3d9018d5ebfa2e1469579e52b3012f1d
magic-doggo <109861946+magic-doggo@users.noreply.github.com>
2025-04-12 08:26:49 +0000

    Retrieve pokemon for sale via API and display on shop page

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,6 +1,30 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Shop = () => {
+    const [shopItems, setShopItems] = useState([]);
+    //read lesson api lesson examples again again
+    
+    useEffect(() => {
+        async function getPokemonForSale(nrOfPokemon) {
+            //could create another argument, pokemonStartingIndex. And use this when user clicks on page 2,3, etc
+            //to show pokemon starting from nrOfPokemon x nr of page
+            let tempShopItems = [];
+            for (let tempIndex = 1; tempIndex < (nrOfPokemon + 1); tempIndex++) {
+                console.log(shopItems, "be4")
+                const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${tempIndex}`);
+                if (!response.ok) {
+                    throw new Error(`Error fetching data for pokemon index ${tempIndex}`)
+                }
+                const shopItemsData = await response.json();
+                let pokemonShopItemObject = { name: shopItemsData.forms[0].name, icon: shopItemsData.sprites.front_default, pokeIndex: tempIndex, price: shopItemsData.stats[0].base_stat}
+                tempShopItems.push(pokemonShopItemObject);
+            }
+            setShopItems(tempShopItems)
+        }
+        getPokemonForSale(3);
+    }, [])
+
     return (
 
         <div>
@@ -12,8 +36,30 @@ const Shop = () => {
                     <Link to="/cart">Cart</Link>
                 </nav>
             </header>
+            <h2>Shop Page</h2>
+            <div>
+                {shopItems.map((item)=>
+                <div>
+                    <div>{item.name}</div>
+                </div>
+                )}
+            </div>
         </div>
     )
 }
 
 export default Shop;
+
+
+//can use stats[0].base_stat as price (it is the hp)
+//can also show abilities (need to loop through array ti get each ability name?)
+
+
+//for individual cards/items, do like rimi: image on top, name underneath, price underneath.
+//under that, a wide button "add to cart". when clicked, show number in middle, - and + button on sides
+
+
+
+
+//OPTIONAL
+//buttons on bottom to scroll to a new page of new items. 12ish items on page, 100 possible pages?
