b9e520508b72dc427f102cb38e01e5da673d0b11
magic-doggo <109861946+magic-doggo@users.noreply.github.com>
2025-04-13 06:20:12 +0000

    Add functionality to add items to shopping cart

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,11 @@
-export default function Card({imageURL, name, price}) {
+export default function Card({ imageURL, name, price, nrOfCopiesInShoppingCart, id, addToCart }) {
     return (
         <div>
             <img src={imageURL} alt="image of pokemon for sale" />
             <div>{name}</div>
             <div>{price}$</div>
-            <button>Add to Cart</button>
+            {(nrOfCopiesInShoppingCart > 0) ? (<div>{nrOfCopiesInShoppingCart}</div>) :
+                (<button onClick={() => addToCart(id)}>Add to Cart</button>)}
         </div>
     )
 }
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -6,24 +6,35 @@ const Shop = () => {
     const [shopItems, setShopItems] = useState([]);
     //read lesson api lesson examples again again
     
+    function addToCart(id) {
+
+        setShopItems(shopItems.map(item => {
+            if (item.pokeIndex === id) {
+                return {...item, nrOfCopiesInShoppingCart: item.nrOfCopiesInShoppingCart+1}
+            } else return item;
+        }))
+        // console.log(pokeItem.pokeIndex)
+    }
+
     useEffect(() => {
         async function getPokemonForSale(nrOfPokemon) {
             //could create another argument, pokemonStartingIndex. And use this when user clicks on page 2,3, etc
             //to show pokemon starting from nrOfPokemon x nr of page
             let tempShopItems = [];
             for (let tempIndex = 1; tempIndex < (nrOfPokemon + 1); tempIndex++) {
-                console.log(shopItems, "be4")
                 const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${tempIndex}`);
                 if (!response.ok) {
                     throw new Error(`Error fetching data for pokemon index ${tempIndex}`)
                 }
                 const shopItemsData = await response.json();
-                let pokemonShopItemObject = { name: shopItemsData.forms[0].name, icon: shopItemsData.sprites.front_default, pokeIndex: tempIndex, price: shopItemsData.stats[0].base_stat}
+                let pokemonShopItemObject = { name: shopItemsData.forms[0].name, icon: shopItemsData.sprites.front_default, pokeIndex: tempIndex, 
+                    price: shopItemsData.stats[0].base_stat, nrOfCopiesInShoppingCart: 0}
+                    //should i track items in shopping cart inside each item? or have a separate state that they get added to?
                 tempShopItems.push(pokemonShopItemObject);
             }
             setShopItems(tempShopItems)
         }
-        getPokemonForSale(3);
+        getPokemonForSale(2);
     }, [])
 
     return (
@@ -40,7 +51,8 @@ const Shop = () => {
             <h2>Shop Page</h2>
             <div>
                 {shopItems.map((item)=>
-                <Card key={item.pokeIndex} imageURL={item.icon} price={item.price} name={item.name}></Card>
+                <Card key={item.pokeIndex} imageURL={item.icon} price={item.price} name={item.name} id={item.pokeIndex}
+                nrOfCopiesInShoppingCart={item.nrOfCopiesInShoppingCart} addToCart={addToCart}></Card>
                 )}
             </div>
         </div>
