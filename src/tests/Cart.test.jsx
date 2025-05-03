07913dfc278a0257a6fa6d7c282f74d604fa7eaa
magic-doggo <109861946+magic-doggo@users.noreply.github.com>
2025-05-03 07:38:47 +0000

    Mock Pay function and complete Cart tests rest of cart outlet context should be tested under APP

--- src/tests/Cart.test.jsx
+++ src/tests/Cart.test.jsx
@@ -3,13 +3,10 @@ import { BrowserRouter } from "react-router-dom";
 import { describe, it, expect, vi } from "vitest";
 import { render, screen, waitFor, waitForElementToBeRemoved } from "@testing-library/react";
 import RenderRouteWithOutletContext from "../components/RenderRouteWithOutletContext";
-
+import userEvent from "@testing-library/user-event";
 import * as ReactRouter from 'react-router-dom';
 import { MemoryRouter } from 'react-router-dom';
 
-
-
-// const { addToCart, removeFromCart, shopItems, setShopItems, priceOfItemsInBasket, setPriceOfItemsInBasket, changeNrOfItemsFromCart, setNrOfItemsInBasket } = useOutletContext();
 const mockOutletContextData = {
     addToCart: vi.fn(),
     removeFromCart: vi.fn(),
@@ -39,4 +36,26 @@ describe("Cart component", () => {
         </RenderRouteWithOutletContext>)
         expect(screen.getByRole('heading', { level: 3 }).textContent).toMatch("Items in your Cart:");
     });
+
+    it('renders items that are in the Shopping Cart', () => {
+        render(<RenderRouteWithOutletContext context={mockOutletContextData}>
+            <Cart />
+        </RenderRouteWithOutletContext>)
+        expect(screen.getByText("name2")).toBeInTheDocument();
+        expect(screen.queryByText("name1")).not.toBeInTheDocument();
+    });
+
+    it('pay button runs emptyCart, which runs setShopItems, setNrOfItemsInBasket and setPriceOfItemsInBasket', async () => {
+        const user = userEvent.setup();
+        render(<RenderRouteWithOutletContext context={mockOutletContextData}>
+            <Cart />
+        </RenderRouteWithOutletContext>)
+        const payButton = screen.getByText("Pay");
+        await user.click(payButton);
+        expect(mockOutletContextData.setShopItems).toHaveBeenCalled();
+        expect(mockOutletContextData.setNrOfItemsInBasket).toHaveBeenCalledWith(0);
+        expect(mockOutletContextData.setPriceOfItemsInBasket).toHaveBeenCalledWith(0);
+    });
+    //Card component already tested in Card.test.jsx, do not need to test +/-?
+    //test priceOfItemsInBasket and all other states directly under App component?
 })
